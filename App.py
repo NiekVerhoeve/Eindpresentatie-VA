cd370b23d322ad3a90313d60627dbc0e92151380
NiekVerhoeve <114068605+NiekVerhoeve@users.noreply.github.com>
2022-11-04 14:22:39 +0100

    Update App.py

--- App.py
+++ App.py
@@ -332,7 +332,7 @@ with col_filter1:
    foliumfilter1 = st.selectbox("Choropleth on/off",("On","Off"))
 
 with col_filter2:
-    st.image(image, caption='Legenda', height=100)
+    st.image(image, caption='Legenda', width=100)
 
 
 #Making the folium map
