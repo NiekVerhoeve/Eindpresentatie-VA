99a87584af84885d9c2bea860a90bde089ba9c0a
lucwinkens <114579934+lucwinkens@users.noreply.github.com>
2022-11-04 14:15:02 +0100

    Update App.py

--- App.py
+++ App.py
@@ -316,7 +316,7 @@ def add_categorical_legend(folium_map, title, colors, labels):
 
 
 #Setting up the page
-image = Image.open('legenda.jpg')
+image = Image.open('legenda.jpg', use_column_width=False)
 
 st.set_page_config(
     layout="wide",
